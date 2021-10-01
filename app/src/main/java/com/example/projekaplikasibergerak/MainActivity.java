4ca7a8a1a76db0969de9d9ea51f91ca07b913eaa
Andi Saputra <andi21262@gmail.com>
2021-10-01 20:40:07 +0700

    Fix Inisiasi projek

--- app/src/main/java/com/example/projekaplikasibergerak/MainActivity.java
+++ app/src/main/java/com/example/projekaplikasibergerak/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.projekaplikasibergerak;
 
 import android.opengl.Visibility;
-//    import android.support.v7.app.AppCompatActivity; coba
+//    import android.support.v7.app.AppCompatActivity; Java nya gabisa
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 //    import android.support.annotation.NonNull;
@@ -39,98 +39,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mButtonPower.setOnClickListener(this);
         mButtonAuto.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_power:
-                toggleLed();
-                break;
-        }
-        switch (view.getId()) {
-            case R.id.button_auto:
-                toggleAuto();
-                break;
-        }
-    }
-
-    void toggleLed() {
-        if (mButtonPower.isChecked()) {
-            turnOnLed();
-        } else {
-            turnOffLed();
-        }
-    }
-
-    void toggleAuto() {
-        if (mButtonAuto.isChecked()) {
-            mButtonPower.setVisibility(View.VISIBLE);
-            turnOnAuto();
-        } else {
-            turnOffAuto();
-        }
-    }
-
-    void turnOnLed() {
-        mButtonPower.setEnabled(false);
-        mDatabase.getReference("manual").
-                setValue(1).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "LAMPU MENYALA", Toast.LENGTH_SHORT).show();
-                    mButtonPower.setChecked(true);
-                    mImageStatus.setImageResource(R.drawable.light_on);
-                }
-                mButtonPower.setEnabled(true);
-            }
-        });
-    }
-
-    void turnOffLed() {
-        mButtonPower.setEnabled(false);
-        mDatabase.getReference("manual").setValue(0).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "LAMPU MATI", Toast.LENGTH_SHORT).show();
-                    mButtonPower.setChecked(false);
-                    mImageStatus.setImageResource(R.drawable.light_off);
-                }
-                mButtonPower.setEnabled(true);
-            }
-        });
-    }
-
-    void turnOnAuto() {
-        mButtonAuto.setEnabled(false);
-        mDatabase.getReference("otomatis").setValue(1).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "FITUR OTOMATIS MENYALA", Toast.LENGTH_SHORT).show();
-                    mButtonAuto.setChecked(true);
-                    mImageStatus.setImageResource(R.drawable.light_off);
-                }
-                mButtonAuto.setEnabled(true);
-            }
-        });
-    }
-
-    void turnOffAuto() {
-        mButtonAuto.setEnabled(false);
-        mDatabase.getReference("otomatis").setValue(0).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "FITUR OTOMATIS MATI", Toast.LENGTH_SHORT).show();
-                    mButtonAuto.setChecked(false);
-                    mImageStatus.setImageResource(R.drawable.light_off);
-                }
-                mButtonAuto.setEnabled(true);
-            }
-        });
     }
 
     @Override
