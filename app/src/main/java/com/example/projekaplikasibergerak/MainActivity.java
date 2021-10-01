5d06c263c6c6f42541d788ca4236338bfbe9a12f
Andi Saputra <86837025+zqarkop@users.noreply.github.com>
2021-10-01 20:37:49 +0700

    Test Java
    Test push error java nya

--- app/src/main/java/com/example/projekaplikasibergerak/MainActivity.java
+++ app/src/main/java/com/example/projekaplikasibergerak/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.projekaplikasibergerak;
 
 import android.opengl.Visibility;
-//    import android.support.v7.app.AppCompatActivity;
+//    import android.support.v7.app.AppCompatActivity; coba
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 //    import android.support.annotation.NonNull;
